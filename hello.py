236eae56e1a927c04fdcfb840c24accef66bdb71
shayzimm <shay.zimmerle@gmail.com>
2024-04-23 11:24:48 +0800

    catch-up

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 my_name = input('What is your name? ')
-my_age = input('What is your age? ')
-print('Hello, ' + my_name + ' you are ' + my_age + ' years old!')
+my_age = int(input('What is your age? '))
+# print('Hello, ' + my_name + ' you are ' + str(my_age + 10) + ' years old in a decade!')
+print(f'Hello, {my_name} you are {my_age + 10} years old in a decade!')
